838452b0808c177103875e6ea07f2938844f62ab
Niyrux <samuel47000@hotmail.com>
2026-04-27 19:15:46 +0200

    problème de jsx-runtime fix npm run dev et npm run preview maintenant fonctionnel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    dedupe: ['react', 'react-dom', 'react/jsx-runtime'],
+    alias: {
+      'react/jsx-runtime': path.resolve('./node_modules/react/jsx-runtime.js'),
+    }
+  }
 })
